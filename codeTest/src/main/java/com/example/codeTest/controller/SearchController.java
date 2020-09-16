3e3a1f434c6dcaaa6cd323d5cd46d38a223bc779
jparkorea <gkshdn@naver.com>
2020-09-17 02:24:19 +0900

    init

---
+++ codeTest/src/main/java/com/example/codeTest/controller/SearchController.java
@@ -0,0 +1,15 @@
+package com.example.codeTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+	@GetMapping("/addr")
+	public String addr() {
+		return "search/addr";
+	}
+}
