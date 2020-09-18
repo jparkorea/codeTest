a53e8eb4c39187388e17d29782bcef2da79780f8
jparkorea <gkshdn@naver.com>
2020-09-18 23:03:59 +0900

    디렉토리 구조 정의 및 라이브러리/로거 추가

--- codeTest/src/main/java/com/example/codeTest/controller/BoardController.java
+++ codeTest/src/main/java/com/example/codeTest/controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.example.codeTest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.codeTest.domain.entity.KeywordEntity;
+import com.example.codeTest.domain.repository.KeywordRepository;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+	@Autowired
+	private KeywordRepository keywordRepository;
+	
 	@GetMapping("/list")
-	public String addr() {
+	public String addr(Model model) {
+		List<KeywordEntity> keywords = keywordRepository.findAll();
+		model.addAttribute("keywords", keywords);
 		return "board/list";
 	}
 }
--- codeTest/src/main/java/com/example/codeTest/controller/HomeController.java
+++ codeTest/src/main/java/com/example/codeTest/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.codeTest.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@GetMapping
 	public String index() {
+		logger.info("HomeController, GetMapping('/')");
 		return "index";
 	}
 }
