834ebad5facd74993188ed5ebef4abe0fbb7fc27
jparkorea <gkshdn@naver.com>
2020-09-21 18:05:55 +0900

    Custom Error Page

---
+++ codeTest/src/main/java/com/example/codeTest/error/CustomErrorController.java
@@ -0,0 +1,40 @@
+package com.example.codeTest.error;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+	private String VIEW_PATH = "/error/";
+
+	@RequestMapping(value = "/error")
+	public String handleError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if (status != null) {
+			int statusCode = Integer.valueOf(status.toString());
+			if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+				return VIEW_PATH + "401";
+			}
+			if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				return VIEW_PATH + "403";
+			}
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return VIEW_PATH + "404";
+			}
+			if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return VIEW_PATH + "500";
+			}
+		}
+		return "error";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+}
